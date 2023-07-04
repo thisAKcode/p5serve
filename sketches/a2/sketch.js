840e382360e3cdf464b6d9e71f9c2b83dca0ecec
thisAKcode <alekseikupiakov@hotmail.com>
2023-07-04 22:37:44 +0200

    some stuff

--- sketches/a2/sketch.js
+++ sketches/a2/sketch.js
@@ -1,10 +1,32 @@
 let img; // Declare variable 'img'.
+let projName = 'a2';
+let pathCheck;
+
+function imgPathChecker() {
+  if (window.location.href == 'http://localhost:8000/') {
+    pathCheck = 'images/image.JPG';
+  } else {
+    pathCheck = 'https://raw.githubusercontent.com/thisAKcode/p5serve/master/sketches/${projName}/images/image.JPG'; // message = `Hello, ${name}! You are ${age} years old.`;
+  }
+  return pathCheck 
+}
 
 function setup() {
   createCanvas(720, 400);
   //C:\p5serve\sketches\a2
-  //img = loadImage('../../images/image.jpg');
-  img = loadImage('https://raw.githubusercontent.com/thisAKcode/p5serve/master/sketches/a2/images/image.JPG'); 
+  //console.log(process.cwd())
+  let pathCheck = 'images/image.JPG';
+  if (window.location.href == 'http://localhost:8000/') {
+    console.log('local file')
+  //  block of code to be executed if the condition is true
+  } else {
+    pathCheck = 'https://raw.githubusercontent.com/thisAKcode/p5serve/master/sketches/${projName}/images/image.JPG'; // message = `Hello, ${name}! You are ${age} years old.`;
+  //  block of code to be executed if the condition is false
+  }
+  pathCheck = imgPathChecker()
+  console.log('here I am',window.location.href);
+  img = loadImage(pathCheck);
+  //img = loadImage('https://raw.githubusercontent.com/thisAKcode/p5serve/master/sketches/a2/images/image.JPG'); 
   //img = loadImage('https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8b3BlbiUyMHNvdXJjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'); // Load the image
 }
 
---
+++ sketches/a4/sketch.js
@@ -0,0 +1,46 @@
+let tileSize = 50; // Size of each tile
+let rows, cols; // Number of rows and columns of tiles
+
+function setup() {
+  createCanvas(500, 500);
+  background(255);
+  
+  // Calculate the number of rows and columns based on canvas size and tile size
+  rows = height / tileSize;
+  cols = width / tileSize;
+  
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
+      let xPos = x * tileSize;
+      let yPos = y * tileSize;
+      
+      // Generate a random number to determine the type of tile
+      let tileType = random(1);
+      
+      // Draw the appropriate tile based on the random number
+      if (tileType < 0.5) {
+        drawTile1(xPos, yPos);
+      } else {
+        drawTile2(xPos, yPos);
+      }
+    }
+  }
+}
+
+// Function to draw tile type 1
+function drawTile1(x, y) {
+  fill(0);
+  rect(x, y, tileSize, tileSize);
+  fill(255);
+  textSize(20);
+  text("X", x + tileSize / 2, y + tileSize / 2);
+}
+
+// Function to draw tile type 2
+function drawTile2(x, y) {
+  fill(255);
+  rect(x, y, tileSize, tileSize);
+  fill(0);
+  textSize(20);
+  text("O", x + tileSize / 2, y + tileSize / 2);
+}
